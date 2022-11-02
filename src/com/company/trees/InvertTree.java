71409d8be41569698b001d26214ccfac0d26e2e7
codewithjaspreet <jaspreetsodhi02@gmail.com>
2022-11-02 15:21:06 +0530

    invert

---
+++ src/com/company/trees/InvertTree.java
@@ -0,0 +1,33 @@
+package com.company.trees;
+
+public class InvertTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+       TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val,TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+        public TreeNode invertTree(TreeNode root) {
+
+            if(root == null){
+                return null;
+            }
+
+
+            else{
+                TreeNode left = invertTree(root.left);
+                TreeNode right = invertTree(root.right);
+
+                root.right = left;
+                root.left  = right;
+                return root;
+
+            }
+    }
+}
