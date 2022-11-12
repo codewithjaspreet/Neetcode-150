e57025d1b3d6f08e25615fabcff164d649122ad3
codewithjaspreet <jaspreetsodhi02@gmail.com>
2022-11-12 21:47:03 +0530

    queue - radial bfs

--- src/com/company/graphs/bfs.java
+++ src/com/company/graphs/bfs.java
@@ -1,41 +1,84 @@
 package com.company.graphs;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class bfs {
+    static class Edge {
+        int src;
+        int nbr;
 
-    public static ArrayList<Integer> bfsTraversal(ArrayList<ArrayList<Integer>> Adj , int V ){
-
-        ArrayList<Integer> ans = new ArrayList<>();
-        Queue<Integer> q = new ArrayDeque<>();
-
-        q.add(0);
-        boolean[] visited = new boolean[V];
-
-        visited[0] = true;
-
-        while (!q.isEmpty()){
-
-            Integer node  = q.poll();
-
-            ans.add(node);
-
-            for (Integer i : Adj.get(node)){
-
-                if (!visited[i]){
-
-                    visited[i] = true;
-                    q.add(i);
-                }
-            }
-
+        Edge(int src, int nbr) {
+            this.src = src;
+            this.nbr = nbr;
         }
-        return  ans;
-
     }
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            graph[v1].add(new Edge(v1, v2));
+            graph[v2].add(new Edge(v2, v1));
+        }
+
+        int src = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Pair> queue = new ArrayDeque<>();
+        queue.add(new Pair(src, src + ""));
+        boolean[] visited = new boolean[vtces];
+        while (queue.size() > 0) {
+            Pair rem = queue.remove();
+
+            if (visited[rem.v]) {
+                continue;
+            }
+            visited[rem.v] = true;
+            System.out.println(rem.v + "@" + rem.psf);
+
+            for (Edge e : graph[rem.v]) {
+                if (!visited[e.nbr]) {
+                    queue.add(new Pair(e.nbr, rem.psf + e.nbr));
+                }
+            }
+        }
+    }
+
+    static class Pair {
+        int v;
+        String psf;
+
+        Pair(int v, String psf) {
+            this.v = v;
+            this.psf = psf;
+        }
+    }
 }
+//}
+//0 1 10
+//
+//        1 2 10
+//
+//        2 3 10
+//
+//        0 3 10
+//
+//        3 4 10
+//
+//        4 5 10
+//
+//        5 6 10
+//
+//        4 6 10
+//
+//        2
